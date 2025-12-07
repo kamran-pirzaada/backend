cd310866ee37c4e7d225e2a1090367ac58555e3e
kamran-pirzaada <kamranpirzaada@gmail.com>
2025-12-07 15:45:49 +0500

    fix: vercel serverless issues1

--- routes/test.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-router.get("/test-env", (req, res) => {
-  res.json({ uri: process.env.MONGODB_URL || "MONGODB_URL not found" });
-});
-
-export default router;
